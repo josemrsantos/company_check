5d457ed36c3a8366b912215ccf0ca33c0289e3ac
José Santos <josemrsantos@gmail.com>
2016-03-06 18:18:32 +0000

    sleep

--- check.py
+++ check.py
@@ -5,12 +5,13 @@ import sys
 import argparse
 import urllib
 import os
+import time
 
 def get_data_from_file(file_name):
     with open(file_name) as f:
         return f.readlines()
 
-def run_chrome(company_list, queries):
+def run_chrome(company_list, queries, sleep="0.1"):
     if os.name=="nt":
         call_chrome = ["start","chrome"]
     else:
@@ -22,6 +23,7 @@ def run_chrome(company_list, queries):
             query_str = j.replace(" ", "+")
             query =  name_str + "+" + query_str
             call(call_chrome + ["https://www.google.co.uk/#q="+query])
+        time.sleep(sleep)
 
 def main():
     """
@@ -32,7 +34,13 @@ def main():
     parser.add_argument("--list", help="File with campany names, line separated.")
     parser.add_argument("--one", help="One company name.")
     parser.add_argument("--queries", help="Queries to do.")
+    parser.add_argument("--sleep", help="Seconds to sleep between companies. default=0.1")
     args = parser.parse_args()
+    # sleep ?
+    if args.sleep:
+        sleep = float(args.sleep)
+    else:
+        sleep = 0.1
     # Where are the queries ?
     if args.queries:
         queries_file = args.queries
@@ -48,7 +56,7 @@ def main():
     else:
         company_list = get_data_from_file("companies.txt")
     # Run all in browser
-    run_chrome(company_list, queries)
+    run_chrome(company_list, queries, sleep)
 
 if __name__ == "__main__":
     main()
